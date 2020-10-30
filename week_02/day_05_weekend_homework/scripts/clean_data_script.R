579e1b26a89a81e033759c1fd324c2eb85f500d6
Harry Williams <hgw1@hotmail.co.uk>
2020-10-30 15:31:41 +0000

    removed NA

--- week_02/day_05_weekend_homework/scripts/clean_data_script.R
+++ week_02/day_05_weekend_homework/scripts/clean_data_script.R
@@ -12,3 +12,24 @@ meteorite_landings <- read_csv("raw_data/meteorite_landings.csv")
 # 2. Clean names ----------------------------------------------------------
 
 meteorite_landings_clean <- clean_names(meteorite_landings)
+
+
+# Split GeoLocation  ------------------------------------------------------
+
+meteorite_landings_clean <- meteorite_landings_clean %>% 
+  mutate(geo_location = str_remove_all(geo_location, "\\(")) %>% 
+  mutate(geo_location = str_remove_all(geo_location, "\\)")) %>% 
+  separate(col = geo_location, 
+           into = c("latitude", "longitude"), 
+           sep =", ") %>% 
+  mutate(latitude = as.numeric(latitude), 
+         longitude = as.numeric(longitude))
+  
+
+# Replace NAs -------------------------------------------------------------
+
+meteorite_landings_clean <- meteorite_landings_clean %>% 
+mutate(
+  latitude = coalesce(latitude, 0),
+  longitude = coalesce(longitude, 0)
+) 
